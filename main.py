4700cb3c69d61a7a1c9c4062ff60650cbf633159
bigname <tuanhiep.tran88@gmail.com>
2021-02-02 23:33:40 -0700

    introduction of pyplot, basic commands

--- main.py
+++ main.py
@@ -1,4 +1,52 @@
+# Intro to pyplot
 import matplotlib.pyplot as plt
+
 plt.plot([1, 2, 3, 4])
 plt.ylabel('some numbers')
 plt.show()
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.show()
+# Formatting the style of your plot
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+plt.axis([0, 6, 0, 20])
+plt.show()
+
+import numpy as np
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
+
+# Plotting with keyword strings
+
+data = {'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)}
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+plt.scatter('a', 'b', c='c', s='d', data=data)
+plt.xlabel('entry a')
+plt.ylabel('entry b')
+plt.show()
+
+# Plotting with categorical variables
+
+names = ['group_a', 'group_b', 'group_c']
+values = [1, 10, 100]
+
+plt.figure(figsize=(9, 3))
+
+plt.subplot(131)
+plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values)
+plt.subplot(133)
+plt.plot(names, values)
+plt.suptitle('Categorical Plotting')
+plt.show()
+
